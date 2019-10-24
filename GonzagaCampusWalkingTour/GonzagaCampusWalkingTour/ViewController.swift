75c2377b296b33fbe238496953f3fd053c778995
maxheinzelman <mheinzelman@zagmail.gonzaga.edu>
2019-10-23 17:28:44 -0700

    initial xcode project with gitignore

---
+++ GonzagaCampusWalkingTour/GonzagaCampusWalkingTour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GonzagaCampusWalkingTour
+//
+//  Created by Max Heinzelman on 10/23/19.
+//  Copyright © 2019 Senior Design Group 8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
