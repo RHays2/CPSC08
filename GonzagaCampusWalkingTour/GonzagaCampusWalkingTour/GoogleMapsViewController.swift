b65fabdaf5ccdaca3bc7a554f4a792456cd9f897
maxheinzelman <mheinzelman@zagmail.gonzaga.edu>
2019-10-28 22:32:08 -0700

    added location services so the map will display current location

--- GonzagaCampusWalkingTour/GonzagaCampusWalkingTour/GoogleMapsViewController.swift
+++ GonzagaCampusWalkingTour/GonzagaCampusWalkingTour/GoogleMapsViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 import GoogleMaps
 
-class GoogleMapsViewController: UIViewController {
+class GoogleMapsViewController: UIViewController,CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // check to make sure the user has location enabled
+        if CLLocationManager.locationServicesEnabled() {
+            print("Location services enabled")
+            setupLocationServices()
+        }
+        else {
+            // the user turned off location, phone is airplane mode, lack of hardware, hardware failure,...
+            print("Location services disabled")
+        }
+    }
+    
+    func setupLocationServices() {
+        locationManager.delegate = self
+        // shows the users location as accurate as possible. Will be battery intensive
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        // requests users location only when the app is in use
+        locationManager.requestWhenInUseAuthorization()
+        //begin getting the location
+        locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,15 +48,19 @@ class GoogleMapsViewController: UIViewController {
         // coordinate -33.86,151.20 at zoom level 6.
         let camera = GMSCameraPosition.camera(withLatitude: 47.667533, longitude: -117.400630, zoom: 17.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        view = mapView
+        //self.view = mapView
         
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: 47.667533, longitude: -117.400630)
         marker.title = "Gonzaga University"
         marker.snippet = "Test Location"
+        mapView.settings.myLocationButton = true
+        mapView.isMyLocationEnabled = true
         marker.map = mapView
     }
+    
+    
 
     /*
     // MARK: - Navigation
