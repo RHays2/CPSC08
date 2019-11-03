389c03657f8a4c73edd3dca4f038811c63e9277b
maxheinzelman <mheinzelman@zagmail.gonzaga.edu>
2019-11-03 09:52:14 -0800

    changed layout to have map displayed correctly on all phone sizes

---
+++ GonzagaCampusWalkingTour/GonzagaCampusWalkingTour/Extensions/UIView+.swift
@@ -0,0 +1,9 @@
+//
+//  UIView+.swift
+//  GonzagaCampusWalkingTour
+//
+//  Created by Max Heinzelman on 11/2/19.
+//  Copyright © 2019 Senior Design Group 8. All rights reserved.
+//
+
+import Foundation
