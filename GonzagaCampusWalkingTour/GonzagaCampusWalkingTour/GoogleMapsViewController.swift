639a1671d48ff0e9d05c71695c2e7480d733fd31
maxheinzelman <mheinzelman@zagmail.gonzaga.edu>
2019-11-06 21:15:53 -0800

    updated map to follow the users orientation and location

--- GonzagaCampusWalkingTour/GonzagaCampusWalkingTour/GoogleMapsViewController.swift
+++ GonzagaCampusWalkingTour/GonzagaCampusWalkingTour/GoogleMapsViewController.swift
@@ -12,6 +12,10 @@ import GoogleMaps
 
 class GoogleMapsViewController: UIViewController,CLLocationManagerDelegate {
     let locationManager = CLLocationManager()
+    var mapView = GMSMapView.map(withFrame: CGRect.zero, camera: GMSCameraPosition.camera(withLatitude: 0, longitude: 0, zoom:0))
+    let CAMERA_ANGLE = 45.0;
+    var currentDirection = CLLocationDirection()
+    var currentLocation = CLLocation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,25 +26,13 @@ class GoogleMapsViewController: UIViewController,CLLocationManagerDelegate {
         if CLLocationManager.locationServicesEnabled() {
             print("Location services enabled")
             setupLocationServices()
+            setUpMapView()
         }
         else {
             // the user turned off location, phone is airplane mode, lack of hardware, hardware failure,...
             print("Location services disabled")
         }
         
-        // Create a GMSCameraPosition that tells the map to display the
-        // centered around the users location
-        let lattitude = locationManager.location?.coordinate.latitude
-        let longitude = locationManager.location?.coordinate.longitude
-        let camera = GMSCameraPosition.camera(withLatitude: lattitude ?? 0.0, longitude: longitude ?? 0.0, zoom: 18.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        mapView.settings.myLocationButton = true
-        mapView.isMyLocationEnabled = true
-        
-        //add layout constraints
-        view.addSubviewAndPinEdges(mapView)
-        
-
         // Creates a marker in the center of the map.
 //        let marker = GMSMarker()
 //        marker.position = CLLocationCoordinate2D(latitude: 47.667533, longitude: -117.400630)
@@ -49,14 +41,43 @@ class GoogleMapsViewController: UIViewController,CLLocationManagerDelegate {
 //        marker.map = mapView
     }
     
+    func centerUserLocationOnMap(location: CLLocation) {
+        // Create a GMSCameraPosition that tells the map to display the
+        // centered around the users location
+        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude, longitude: location.coordinate.longitude, zoom: 18.0, bearing: self.currentDirection, viewingAngle: CAMERA_ANGLE)
+        mapView.animate(to: camera)
+        
+        //update current location var
+        self.currentLocation = location
+        
+    }
+    
+    func orientMapTowardUserHeading(direction: CLLocationDirection) {
+        //orient the map in the direction the user is facing
+        mapView.animate(toBearing: direction)
+        //update current direction
+        self.currentDirection = direction
+    }
+    
     func setupLocationServices() {
         locationManager.delegate = self
         // shows the users location as accurate as possible. Will be battery intensive
         locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
         // requests users location only when the app is in use
         locationManager.requestWhenInUseAuthorization()
-        //begin getting the location
+        //begin getting the location and heading
         locationManager.startUpdatingLocation()
+        locationManager.startUpdatingHeading();
+    }
+    
+    func setUpMapView() {
+        //enable locations on map
+        mapView.settings.myLocationButton = true
+        mapView.isMyLocationEnabled = true
+        //change map type to be hybrid (satellite with labels)
+        mapView.mapType = GMSMapViewType.hybrid
+        //add layout constraints
+        view.addSubviewAndPinEdges(mapView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -65,7 +86,12 @@ class GoogleMapsViewController: UIViewController,CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-
+        //move map to center around user
+        self.centerUserLocationOnMap(location: locations.last ?? CLLocation())
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        self.orientMapTowardUserHeading(direction: newHeading.magneticHeading)
     }
 }
 
